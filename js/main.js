5fcdd5a83f6c71bcb4ca690040421147b1b3a1ec
Niko Pastore <nikopastore@gmail.com>
2024-11-08 17:09:44 -0800

    Update main.js

--- js/main.js
+++ js/main.js
@@ -36,91 +36,93 @@ d3.csv("data/GDP_annual_growth_NEW.csv")
         console.error("Error loading the CSV file:", error);
     });
 
-    function createVisualization(data) {
-        const margin = { top: 50, right: 150, bottom: 50, left: 100 };
-        const width = 800 - margin.left - margin.right;
-        const height = 400 - margin.top - margin.bottom;
-    
-        const svg = d3.select("#visualization")
-            .append("svg")
-            .attr("width", width + margin.left + margin.right)
-            .attr("height", height + margin.top + margin.bottom)
-            .append("g")
-            .attr("transform", `translate(${margin.left},${margin.top})`);
-    
-        const xScale = d3.scaleLinear()
-            .domain([2000, 2020])
-            .range([0, width]);
-    
-        const yScale = d3.scaleLinear()
-            .domain(d3.extent(data, d => d.gdp_growth))
-            .range([height, 0]);
-    
-        const xAxis = d3.axisBottom(xScale).tickFormat(d3.format("d"));
-        const yAxis = d3.axisLeft(yScale);
-    
-        svg.append("g")
-            .attr("transform", `translate(0, ${height})`)
-            .call(xAxis);
-    
-        svg.append("g")
-            .call(yAxis);
-    
-        const line = d3.line()
-            .x(d => xScale(d.year))
-            .y(d => yScale(d.gdp_growth));
-    
-        const nestedData = d3.group(data, d => d.country);
-    
-        const tooltip = d3.select("#tooltip");
-    
-        svg.selectAll(".line")
-            .data(nestedData)
-            .enter()
-            .append("path")
-            .attr("class", "line")
-            .attr("d", d => line(d[1]))
-            .style("fill", "none")
-            .style("stroke", (d, i) => d3.schemeCategory10[i % 10])
-            .style("stroke-width", 1.5)
-            .on("mouseover", function (event, d) {
-                d3.select(this)
-                    .style("stroke-width", 3)
-                    .style("stroke", "orange");
-    
-                tooltip.style("visibility", "visible")
-                    .html(`<strong>Country:</strong> ${d[0]}`);
-            })
-            .on("mousemove", function (event) {
-                tooltip.style("top", (event.pageY - 10) + "px")
-                    .style("left", (event.pageX + 10) + "px");
-            })
-            .on("mouseout", function () {
-                d3.select(this)
-                    .style("stroke-width", 1.5)
-                    .style("stroke", (d, i) => d3.schemeCategory10[i % 10]);
-    
-                tooltip.style("visibility", "hidden");
-            });
-    
-        const legend = svg.selectAll(".legend")
-            .data(nestedData.keys())
-            .enter()
-            .append("g")
-            .attr("class", "legend")
-            .attr("transform", (d, i) => `translate(0,${i * 20})`);
-    
-        legend.append("rect")
-            .attr("x", width + 20)
-            .attr("width", 10)
-            .attr("height", 10)
-            .style("fill", (d, i) => d3.schemeCategory10[i % 10]);
-    
-        legend.append("text")
-            .attr("x", width + 35)
-            .attr("y", 5)
-            .attr("dy", ".35em")
-            .style("text-anchor", "start")
-            .text(d => d);
-    }
-    
+function createVisualization(data) {
+    const margin = { top: 50, right: 150, bottom: 50, left: 100 };
+    const width = 800 - margin.left - margin.right;
+    const height = 400 - margin.top - margin.bottom;
+
+    const svg = d3.select("#visualization")
+        .append("svg")
+        .attr("width", width + margin.left + margin.right)
+        .attr("height", height + margin.top + margin.bottom)
+        .append("g")
+        .attr("transform", `translate(${margin.left},${margin.top})`);
+
+    const xScale = d3.scaleLinear()
+        .domain([2000, 2020])
+        .range([0, width]);
+
+    const yScale = d3.scaleLinear()
+        .domain(d3.extent(data, d => d.gdp_growth))
+        .range([height, 0]);
+
+    const xAxis = d3.axisBottom(xScale).tickFormat(d3.format("d"));
+    const yAxis = d3.axisLeft(yScale);
+
+    svg.append("g")
+        .attr("transform", `translate(0, ${height})`)
+        .call(xAxis);
+
+    svg.append("g")
+        .call(yAxis);
+
+    const line = d3.line()
+        .x(d => xScale(d.year))
+        .y(d => yScale(d.gdp_growth));
+
+    const nestedData = d3.group(data, d => d.country);
+
+    const tooltip = d3.select("#tooltip");
+
+    svg.selectAll(".line")
+        .data(nestedData)
+        .enter()
+        .append("path")
+        .attr("class", "line")
+        .attr("d", d => line(d[1]))
+        .style("fill", "none")
+        .style("stroke", (d, i) => d3.schemeCategory10[i % 10])
+        .style("stroke-width", 1.5)
+        .each(function (d, i) {
+            d.originalColor = d3.schemeCategory10[i % 10];
+        })
+        .on("mouseover", function (event, d) {
+            d3.select(this)
+                .style("stroke-width", 3)
+                .style("stroke", "orange");
+
+            tooltip.style("visibility", "visible")
+                .html(`<strong>Country:</strong> ${d[0]}`);
+        })
+        .on("mousemove", function (event) {
+            tooltip.style("top", (event.pageY - 10) + "px")
+                .style("left", (event.pageX + 10) + "px");
+        })
+        .on("mouseout", function (event, d) {
+            d3.select(this)
+                .style("stroke-width", 1.5)
+                .style("stroke", d.originalColor);
+
+            tooltip.style("visibility", "hidden");
+        });
+
+    const legend = svg.selectAll(".legend")
+        .data(nestedData.keys())
+        .enter()
+        .append("g")
+        .attr("class", "legend")
+        .attr("transform", (d, i) => `translate(0,${i * 20})`);
+
+    legend.append("rect")
+        .attr("x", width + 20)
+        .attr("width", 10)
+        .attr("height", 10)
+        .style("fill", (d, i) => d3.schemeCategory10[i % 10]);
+
+    legend.append("text")
+        .attr("x", width + 35)
+        .attr("y", 5)
+        .attr("dy", ".35em")
+        .style("text-anchor", "start")
+        .text(d => d);
+}
